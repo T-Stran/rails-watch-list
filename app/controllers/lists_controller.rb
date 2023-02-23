aba29bd6249fc08b4e5f5541a1fcfed57fff7ba5
Tom Stranack <tomstranack@yahoo.co.uk>
2023-02-23 17:00:17 +0000

    controllers finished

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,25 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.required(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,18 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'lists#index'
   # lists routes
-  get 'lists', to: 'lists#index'
-  get 'lists/:id', to: 'lists#show'
-  get 'lists/new', to: 'lists#new'
-  post 'lists', to: 'lists#create'
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show'
+  # post 'lists', to: 'lists#create'
+  resources :lists, only: [:new, :show, :index, :create]
+
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
